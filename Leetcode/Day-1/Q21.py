3331846b5577540dbdeb5842ee7cbf4f9f552108
charliehou1920 <charliehou308@gmail.com>
2025-09-30 16:25:08 -0400

    leetcode 21

--- Leetcode/Day-1/Q21.py
+++ Leetcode/Day-1/Q21.py
@@ -7,3 +7,19 @@ Merge the two lists into one sorted list. The list should be made by splicing to
 Return the head of the merged linked list.
 
 """
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def mergeTwoLists(list1, list2):
+    if list1 is None:
+        return list2
+    elif list2 is None:
+        return list1
+    elif list1.val < list2.val:
+        list1.next = mergeTwoLists(list1.next, list2)
+        return list1
+    else:
+        list2.next = mergeTwoLists(list1, list2.next)
+        return list2
