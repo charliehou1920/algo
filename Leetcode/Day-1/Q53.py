845f3c75b19d9b4b0e09a3aba80fcef77b4580ef
charliehou1920 <charliehou308@gmail.com>
2025-09-30 22:33:20 -0400

    update

--- Leetcode/Day-1/Q53.py
+++ Leetcode/Day-1/Q53.py
@@ -4,7 +4,7 @@ Given an integer array nums,
 find the subarray with the largest sum, and return its sum.
 '''
 
-def maxSubArray(nums):
+def max_noncontinue_elements(nums):
     total_sum = sum(nums)
     result = nums
     for num in nums:
@@ -13,6 +13,11 @@ def maxSubArray(nums):
 
     return sum(result)
 
+def maxSubArra(nums):
+    max_array = current_array = nums[0]
+    for i in range(1, len(nums)):
+        
+
 nums = [-2,1,-3,4,-1,2,1,-5,4]
-result = maxSubArray(nums)
+result = max_noncontinue_elements(nums)
 print(result)
