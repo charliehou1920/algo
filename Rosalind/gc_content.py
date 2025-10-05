1413db6080d97243e0b01e7573479445e44b626b
charliehou1920 <charliehou308@gmail.com>
2025-10-05 16:54:39 -0400

    update

--- Rosalind/gc_content.py
+++ Rosalind/gc_content.py
@@ -1,2 +1,27 @@
 def gc_count(text_path):
-    pass
+    results = {}
+    current_id = None
+    current_seq = ""
+
+    with open(text_path, "r") as f:
+        for line in f:
+            line = line.strip()
+
+            if line.startswith(">Rosalind_"):
+                # If we already have a previous sequence, process it
+                if current_id is not None:
+                    results[current_id] = counting(current_seq)
+                
+                # start a new sequence
+                current_id = line[:1]
+                current_seq = ""
+            else:
+                # The following are DNA sequences
+                current_seq += line
+            
+        if current_id is not None:
+            results["current_id"] = counting(current_seq)
+            
+
+def counting(seq):
+    return (seq.count("G") + seq.count("C")) / len(seq) * 100 
